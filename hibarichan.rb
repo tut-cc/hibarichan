825db2c1a22db49921f3739dc32b0215c8e5c21e
yantene <contact@yantene.net>
2015-02-09 01:11:55 +0900

    オブジェクトの種類ごとの振る舞いを関数化

--- hibarichan.rb
+++ hibarichan.rb
@@ -27,37 +27,42 @@ module Hibarichan
 
     def run
       @stream.user do |object|
-        case object
-        when Twitter::Tweet
-          if object.retweeted_tweet?
-            # 誰かのリツイートを受信した
-            @pmanager.on_retweet(object)
-          elsif object.in_reply_to_user_id == @my_id
-            # 自分宛のツイートを受信した
-            @pmanager.on_reply(object)
-          elsif object.attrs[:user][:id] != @my_id
-            # (自分のツイートを除く)その他のツイートを受信した
-            @pmanager.on_tweet(object)
-          end
-        when Twitter::DirectMessage
-          # ダイレクトメッセージを受信した
-          @pmanager.on_dmessage(object)
-        when Twitter::Streaming::DeletedTweet
-          # ツイートが削除された
-          @pmanager.on_delete(object)
-        when Twitter::Streaming::Event
-          # イベントが発生した
-          @pmanager.on_event(object)
-        when Twitter::Streaming::FriendList
-          # フレンドリストを受信した
-          @pmanager.on_friendlist(object)
-        when Twitter::Streaming::StallWarning
-          # よくわからない．なんかの警告でしょ(適当)
-          @pmanager.on_stallwarning(object)
-        end
+        # オブジェクトの種類ごとに動作する
+        operate(object)
 
         # リポジトリの自動セーブ
         @repository.auto_save
+      end
+    end
+
+    def operate(object)
+      case object
+      when Twitter::Tweet
+        if object.retweeted_tweet?
+          # 誰かのリツイートを受信した
+          @pmanager.on_retweet(object)
+        elsif object.in_reply_to_user_id == @my_id
+          # 自分宛のツイートを受信した
+          @pmanager.on_reply(object)
+        elsif object.attrs[:user][:id] != @my_id
+          # (自分のツイートを除く)その他のツイートを受信した
+          @pmanager.on_tweet(object)
+        end
+      when Twitter::DirectMessage
+        # ダイレクトメッセージを受信した
+        @pmanager.on_dmessage(object)
+      when Twitter::Streaming::DeletedTweet
+        # ツイートが削除された
+        @pmanager.on_delete(object)
+      when Twitter::Streaming::Event
+        # イベントが発生した
+        @pmanager.on_event(object)
+      when Twitter::Streaming::FriendList
+        # フレンドリストを受信した
+        @pmanager.on_friendlist(object)
+      when Twitter::Streaming::StallWarning
+        # よくわからない．なんかの警告でしょ(適当)
+        @pmanager.on_stallwarning(object)
       end
     end
   end
