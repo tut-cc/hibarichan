3cf18ee393aa6be2ee2a94be703266d9b054a854
yantene <contact@yantene.net>
2015-08-17 07:18:55 +0900

    プラグインについても Rubocop 対応した
    色々やった．

--- plugins/speech_center.rb
+++ plugins/speech_center.rb
@@ -1,32 +1,26 @@
 module Hibarichan
   class SpeechCenter < Plugin
-
     # ツイートの生成が出来なかった際に，
     # もう一度それを試みるまでの間隔
-    RetryInterval = 10
-    DefaultFriendsCount = 200
+    RETRY_INTERVAL = 10
+    DEFAULT_FRIENDS_COUNT = 200
 
-    def get_next_interval
+    def next_interval
       # Box-Muller法を用い，
       # フォロー数 * repo('freq_coef')を平均，
       # 平均 / 3を標準偏差とする正規乱数を生成
-
-      # 結果が負の数になることも多いが，
-      # 連投ツイートが行われることは自然であるため，
-      # そのままにしている．
-
       begin
         m = @rest.user.friends_count * (repo['freq_coef'] || 1)
       rescue Twitter::Error::TooManyRequests => e
         p e
       end
 
-      m ||= DefaultFriendsCount
+      m ||= DEFAULT_FRIENDS_COUNT
 
       s = m / 3
 
-      (s * Math::sqrt(-2 * Math::log(rand)) *
-           Math::sin(2 * Math::PI * rand) + m).round
+      (s * Math.sqrt(-2 * Math.log(rand)) *
+           Math.sin(2 * Math::PI * rand) + m).round
     end
 
     def on_tweet(tweet)
@@ -46,22 +40,23 @@ module Hibarichan
       # インターバルを減算
       @tweet_interval = (@tweet_interval || 0) - 1
 
-      # インターバルを終えていたら
-      if @tweet_interval < 0
-        # ツイートする文字列生成
-        begin
-          sentence = get_sentence
-        rescue
-          # 文字列生成に失敗していたら
-          # 次回のインターバルを早める
-          @tweet_interval = RetryInterval
-        else
-          # 文字列生成に成功していればツイート
-          update sentence
+      # インターバルを超えていなければ
+      # そのまま終了
+      return if @tweet_interval >= 0
 
-          # インターバル値の更新
-          @tweet_interval = get_next_interval
-        end
+      # ツイートする文字列生成
+      begin
+        sentence = get_sentence
+      rescue
+        # 文字列生成に失敗していたら
+        # 次回のインターバルを早める
+        @tweet_interval = RETRY_INTERVAL
+      else
+        # 文字列生成に成功していればツイート
+        update sentence
+
+        # インターバル値の更新
+        @tweet_interval = next_interval
       end
     end
   end
